aedea1de4f99dee124b3e1d5f5bcd1c9beef490f
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2025-01-20 12:16:24 +0530

    fixed .env error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
 import { MongoClient, ServerApiVersion } from "mongodb";
 
+dotenv.config();
 const uri = process.env.MONGODB_URI!;
 const dbName = process.env.MONGODB_DB!;
 
