289d2940070644e1bfc4cb94077a57edb5093089
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2025-01-20 11:53:46 +0530

    TS update useFetch

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 
-const useFetch = (url: string) => {
-  const [data, setData] = useState(null);
+const useFetch = <T = any>(
+  url: string
+): { data: T | null; error: Error | null; loading: boolean } => {
+  const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState(true);
 
